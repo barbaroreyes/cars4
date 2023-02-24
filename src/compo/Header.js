74f79ea6ed67796b0a4ce9ca21eb408dd31c533d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-02-24 11:30:01 -0600

    added-header

---
+++ src/compo/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import {AiOutlineSearch  } from 'react-icons/ai';
+import {AiOutlineMenuUnfold} from 'react-icons/ai'
+import Logo from './logo.png';
+const Header = () => {
+  return (
+    <div className="header-main">
+    
+     <div className='menu-container' ><AiOutlineMenuUnfold style={{width:"100%"}}/></div>
+     < div className='logo-container'>
+       <img src ={Logo} alt= '' />
+     </div>
+     <div className='search-container' ><AiOutlineSearch/></div>
+    </div>
+  );
+}
+
+export default Header;
