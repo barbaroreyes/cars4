37544e40d6ba775b73fbc122d48e3cdfa7cbfb07
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-02-24 12:06:40 -0600

    update

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -7,10 +7,21 @@ const Header = () => {
     <div className="header-main">
     
      <div className='menu-container' ><AiOutlineMenuUnfold style={{width:"100%"}}/></div>
+     <div className='menu-container-long' >
+        <a href = "#">Home</a>
+        <a href = "#">Rides</a>
+        <a href = "#">Services</a>
+        <a href = "#">Contact</a>
+        
+     </div>
+
      < div className='logo-container'>
        <img src ={Logo} alt= '' />
      </div>
-     <div className='search-container' ><AiOutlineSearch/></div>
+     <div className='search-container' >
+        <AiOutlineSearch/>
+        <input type='search'/>
+     </div>
     </div>
   );
 }
