9d79420ccb2631e1da65aef0b364b627fd90e090
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-03-08 12:31:25 -0600

    update-home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import {useNavigate} from 'react-router-dom';
 import Picture from '../portada.jpg'
 import Corona from '../corona.png'
 const Home = () => {
-  return (
+  const navigate = useNavigate()
+    return (
     <div className='home-container'>
       <div className='home-container-image1'>
         <img src ={Corona} alt="pictur"/>
@@ -10,7 +12,7 @@ const Home = () => {
       <div className='home-container-image'>
         <img src ={Picture} alt="pictur"/>
       </div>
-      <button className='btn'>Ride Now</button>
+      <button className='btn' onClick={()=> navigate('/login')}>Login</button>
     </div>
   );
 }
