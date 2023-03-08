7213089f7ba55510926ac596e4d82b3d5887efdc
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-03-07 18:27:19 -0600

    update

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import {useState}from 'react';
 import {AiOutlineSearch  } from 'react-icons/ai';
 import {AiOutlineMenuUnfold} from 'react-icons/ai'
 import Logo from './logo.png';
 const Header = () => {
+  const [open ,setOpen] = useState(false)
   return (
     <div className="header-main">
     
-     <div className='menu-container' ><AiOutlineMenuUnfold style={{width:"35px",fontSize:"30px"}}/></div>
+     <div className='menu-container' onClick={()=> console.log("hello")}><AiOutlineMenuUnfold style={{width:"35px",fontSize:"30px"}}/>
+     </div>
      <div className='menu-container-long' >
         <a href = "#">Home</a>
         <a href = "#">Rides</a>
