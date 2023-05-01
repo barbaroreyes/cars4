8e296c2bb3bf2d4e89f6cf2edb53a149fd979db2
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-05-01 13:05:39 -0500

    update-form

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -3,7 +3,9 @@ import React from 'react';
 const Form = () => {
   return (
     <div className='formContainer'>
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeZ90PoUAoGfKhtmZN0XXq8mINXridCPkLyI-vkvGdp3ieTOg/viewform?embedded=true" width="100%" height="520" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+           <form>
+           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeZ90PoUAoGfKhtmZN0XXq8mINXridCPkLyI-vkvGdp3ieTOg/viewform?embedded=true" width="100%" height="520" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+           </form> 
     </div>
   );
 }
