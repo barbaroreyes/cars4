c6bdd62cc5d5ace316fecf591e15812d2efb0549
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-05-01 14:09:49 -0500

    update-home-page

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import {Routes ,Route} from 'react-router-dom'
 import './App.css';
 import Home from './pages/Home';
 import Form from './pages/Form';
+import Header from './compo/Header';
 
 function App() {
   return (
     <div className='container'>
+      <Header/>
     <Routes>
     <Route path="/" element ={<Home/>}/>
     <Route path="/form" element ={<Form />}/>
--- src/pages/Form.js
+++ src/pages/Form.js
@@ -4,7 +4,9 @@ const Form = () => {
   return (
     <div className='formContainer'>
            <form>
-           <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeZ90PoUAoGfKhtmZN0XXq8mINXridCPkLyI-vkvGdp3ieTOg/viewform?embedded=true" width="100%" height="520" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+           <iframe 
+           src="https://docs.google.com/forms/d/e/1FAIpQLSeZ90PoUAoGfKhtmZN0XXq8mINXridCPkLyI-vkvGdp3ieTOg/viewform?embedded=true" width="100%" height="520" frameborder="0" marginheight="0" marginwidth="0">
+            Cargando…</iframe>
            </form> 
     </div>
   );
