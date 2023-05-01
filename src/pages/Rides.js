09dbfdf2f2c34933a72af76c4b4468f6a698888c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2023-05-01 18:15:26 -0500

    update-context

--- src/pages/Rides.js
+++ src/pages/Rides.js
@@ -1,27 +1,17 @@
 import {useState,useContext} from 'react';
-
+import {RideContext} from '../context/ridesContext'
 const Rides = () => {
+    const rides = useContext(RideContext)
+    console.log(rides)
   return (
     <div className='containers'>
       <div className='rides'>
-        <div className='ride'>
-            <span>Reserve</span>
-        </div>
-        <div className='ride'>
-            <span>Ride-Name</span>
-        </div>
-        <div className='ride'>
-            <span>Ride-Name</span>
-        </div>
-        <div className='ride'>
-            <span>Ride-Name</span>
-        </div>
-        <div className='ride'>
-            <span>Ride-Name</span>
-        </div>
-        <div className='ride'>
-            <span>Ride-Name</span>
-        </div>
+        {rides.map((item,i)=>{
+          return( <div className='ride'>
+            <span>{item.name}</span>
+        </div>)
+        })}
+       
       </div>
     </div>
   );
