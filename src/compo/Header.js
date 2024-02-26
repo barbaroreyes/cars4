cd9b81c0277ec7eb0a37c1093e4c8da2f568ea50
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-02-26 16:29:59 -0600

    update-home-page

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -2,18 +2,18 @@ import {useState}from 'react';
 
 import {Link} from 'react-router-dom'
 import Logo from './Logo/Logo';
-import Nav from './Nav/Nav';
+import  Navbar from './Nav/Nav';
 import Search from './Search/Search';
 
 const Header = () => {
-  const [open ,setOpen] = useState(false)
+  
   return (
     <div className="header-main">
     
-    <Nav/>
-   <Search/>
+    <Navbar/>
+    <Search/>
    
-   <>Login</>
+   <h3>Login</h3>
     
     </div>
   );
--- src/compo/Nav/Nav.js
+++ src/compo/Nav/Nav.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { IoIosMenu } from "react-icons/io";
 import './nav.css'
+
 const Nav = () => {
   return (
     <div className='navigation'>
       <IoIosMenu fontSize={45} />
+      <div className='nav-log'>
+       <a>Home</a>
+      </div>
     </div>
   );
 }
