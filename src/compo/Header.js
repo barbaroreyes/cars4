c20aa3b0c4dc593dd99d78f61876a25811e19c7d
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-02-25 22:09:56 -0600

    update-search

--- src/compo/Header.js
+++ src/compo/Header.js
@@ -4,7 +4,7 @@ import {AiOutlineMenuUnfold} from 'react-icons/ai'
 import {Link} from 'react-router-dom'
 import Logo from './Logo/Logo';
 import Nav from './Nav/Nav';
-import Search from '../Search/Search';
+import Search from './Search/Search';
 
 const Header = () => {
   const [open ,setOpen] = useState(false)
--- src/compo/Search/Search.js
+++ src/compo/Search/Search.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { IoIosSearch } from "react-icons/io";
+import './search.css'
 
 const Search = () => {
   return (
-    <div>
-      <IoIosSearch  fontSize={30}/>
+    <div className="search">
+        <input type='text' placeholder='Search a category'/>
+      <IoIosSearch fontSize={15} />
     </div>
   );
 }
