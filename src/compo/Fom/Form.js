9abfdaaa3e5627e1830dc1ee4924ccf82e22257c
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-03-04 23:56:14 -0600

    added-form

--- src/compo/Fom/Form.js
+++ src/compo/Fom/Form.js
@@ -1,16 +1,35 @@
-import React from 'react';
+import {useState}  from 'react';
 import './form.css'
 const Form = () => {
+  const initialState= {
+    name :"",
+    last : "",
+    email:"",
+    phone:"",
+    addressPickUp:"",
+    addressDropeOff:"",
+
+
+
+  }
+  const [book , setBook] = useState(initialState);
+
+  const handleChange = (e) => {
+    setBook((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    
+  };
+
+  console.log("book",book)
   return (
     <form>
         
-          <h1>Book Instantly</h1>
-        <input type="text" name="name" placeholder="Name"/>
-       <input type="text" name="last" placeholder="LastName"/>
-       <input type="email" name="email" placeholder="Email"/>
-       <input type="phone" name="phone" placeholder="Phone"/>
-       <input type="address" name="address" placeholder="Address PickUp"/>  
-      <input type="address" name="address" placeholder="Address Drope Off"/> 
+      <h1>Book Instantly</h1>
+      <input type="text" name="name" placeholder="Name"  onChange={handleChange}/>
+      <input type="text" name="last" placeholder="LastName" onChange={handleChange}/>
+      <input type="email" name="email" placeholder="Email" onChange={handleChange}/>
+       <input type="number" name="phone" placeholder="Phone" onChange={handleChange}/>
+       <input type="address" name="addressPickUp" placeholder="Address PickUp"  onChange={handleChange}/>  
+      <input type="address" name="addressDropeOff" placeholder="Address Drope Off" onChange={handleChange}/> 
       <div class="select">
       <label for="cars">Passagers:</label>
     <select name="passagers" id="passagers">
@@ -27,7 +46,7 @@ const Form = () => {
       </div>
      
           <div className = "btn-container" >
-          <button type="submit"  class="btn btn-primary"name="submit">Book</button>
+          <button type="submit"  className ="btn btn-primary"name="submit">Book</button>
           </div>
           
     </form>
