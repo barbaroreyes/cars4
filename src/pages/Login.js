0860ab27d59f5fdb414d29d9370b0ddb7ec9b3fa
barbaro <48423441+barbaroreyes@users.noreply.github.com>
2024-03-19 23:50:22 -0500

    update-amplify

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -2,9 +2,7 @@ import {Amplify } from 'aws-amplify';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from '../amplifyconfiguration.json';
-
 import Services from './Services';
-import Rides from './Rides';
 Amplify.configure(config);
 
 function Login({ signOut, user }) {
@@ -12,7 +10,7 @@ function Login({ signOut, user }) {
     <>
       <h1>Hello {user.name}</h1>
       <button onClick={signOut}>Sign out</button>
-      <Services/>
+      
     </>
   );
 }
